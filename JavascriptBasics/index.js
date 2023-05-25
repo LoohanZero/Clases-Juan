10b231dc25f100528cb2fc2389ed7a9484e3006f
Lujan Sanchez Cracco <lsanchez@visual-engin.com>
2023-05-25 21:37:55 +0200

    Arrays, bucle for y switch

--- JavascriptBasics/index.js
+++ JavascriptBasics/index.js
@@ -6,7 +6,7 @@ const otroNombre = "Juan";
 let tercerNombre = "Carlos";
 tercerNombre = 0;
 // USAR SIEMPRE CONST A MENOS QUE SEPAS QUE LA VARIABLE VA A CAMBIAR
-console.log(tercerNombre);
+// console.log(tercerNombre);
 
 // --------------------------------------------------------
 // LEVEMENTE TIPADO - A JAVASCRIPT NO LE IMPORTAN LOS TIPOS
@@ -36,12 +36,12 @@ const object = {
 
 function mostrarEnConsola() {
   const cualquierCosa = "cualquier cosa";
-  console.log(cualquierCosa);
+  // console.log(cualquierCosa);
 }
 // DECLARACION - FORMA NUEVA
 const mostrarOtraCosaEnConsola = () => {
   const cualquierCosa = "cualquier otra cosa";
-  console.log(cualquierCosa);
+  // console.log(cualquierCosa);
 };
 
 const devolverAlgo = (name) => {
@@ -54,7 +54,74 @@ const devolverAlgo = (name) => {
 mostrarEnConsola();
 mostrarOtraCosaEnConsola();
 const valor = devolverAlgo("Luajn")
-console.log(valor)
+// console.log(valor)
 //console.log(cualquierCosa) // COMO ESTA DECLARADA DENTRO DE LA FUNCION, NO EXISTE FUERA ENTONCES PARA JS NO ESTA DEFINIDA
 
 //-------------------------------------
+
+//ARRAYS
+
+
+const frutas = [ "banana", "manzana", "pera" ]
+const verdaderos = [ true, true, true, true, false]
+// index a partir del 0 (no del 1), cada lugar del array es un numero creciente de index
+// TIPO RARO DE OBJETO - METODOS ESPECIFICOS PARA ARRAYS
+
+// PUSH - POP
+// PUSH - meter un elemento al final del array
+frutas.push("naranja") // --> YO LO NECESITO COMO DESARROLLADOR --> MAS SE USA
+
+// POP
+frutas.pop() // QUITA ELEMENTO DEL FINAL DEL ARRAY
+
+
+// SHIFT - UNSHIFT --> PRINCIPIO DEL ARRAY -------- CASI NO SE USA
+frutas.unshift("naranja") // --> YO NECESITO PONER NARANJA AL PRINCIPIO DEL ARRAY
+frutas.shift() // --> QUITA ELEMENTO DEL PRINCIPIO DEL ARRAY
+
+// --------------------------------------------------
+const list = [ 2, 5, 7, 8, 15, 23, 54 ] // --> LENGTH = 6
+const secondList = list;
+const menoresADiez = []; 
+
+for (let index = 0; index < list.length; index++) {
+  // console.log("index", index)
+  // console.log("valor", list[index])
+  if(list[index] < 10) {
+    menoresADiez.push(list[index])
+  }
+}
+
+secondList.push("banana")
+// console.log('segunda', secondList);
+// console.log('primera', list)
+
+let miFrutaFavorita;
+
+for (let index = 0; index < frutas.length; index++) {
+  console.log("index", index)
+  console.log("valor", frutas[index])
+  if(frutas[index] === 'manzana') {
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>", frutas[index])
+    miFrutaFavorita = frutas[index]
+    break // --> FRENA EL BUCLE CUANDO ENCUENTRA LO QUE QUIERO
+  }
+}
+
+// console.log('miFrutaFavorita', miFrutaFavorita)
+
+// ---------- SWITCH --------------------
+// STATEMENT - CONDICIONAL ENCADENADO
+const numero = 2;
+
+switch(numero) {
+    case 2: console.log("es par");
+    break
+    case 1: console.log("es impar")
+}
+
+if(numero === 2) {
+  console.log("es par");
+} else if (numero === 1) {
+
+}
